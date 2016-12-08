c7e751cbb38e29a599085a71e1d6b45b71ed1ce7
prashantS47 <prashant3947@gmail.com>
2016-12-08 11:37:28 +0530

    Update patientInfo.php

--- patientInfo.php
+++ patientInfo.php
@@ -1,26 +1,30 @@
 <?php
-	 header('Access-Control-Allow-Origin: *'); 
-	//echo "i am alive";
-	
-	$uri = $_SERVER['HTTP_HOST'];
+	header('Access-Control-Allow-Origin: *'); 
 	$conn = mysql_connect('sql6.freesqldatabase.com', 'sql6147862', '9EaGabMn8E') or die (mysql_error());
-	//echo "connected";
 	@mysql_select_db('sql6147862') or die("Unable to select database");
-	$sql = "SELECT * from patient";
-
-    $result = mysql_query($sql);
+	$rightLimit=10;
+	$count=0;
+	$leftLimit= $_GET['pageNumber'];
+	if($leftLimit!=1){
+	$leftLimit=($leftLimit-1)*10;
+	}
+    else{
+          $countQuery="select count(*) as count from patient";
+	  $resultcount = mysql_query($countQuery);
+          while($row =mysql_fetch_array($resultcount)) 
+          $count=$row['count'];
+	   $leftLimit=0;
+	}
+	$sql = "SELECT * from patient limit ".$leftLimit.",".$rightLimit;
+        $result= mysql_query($sql);
 	
-	$patientDetailsArray=array('patientDetails'=>array());
+	$patientDetailsArray=array('patientDetails'=>array(),'count'=>$count);
 	$arr=array();
 	
-    while($row =mysql_fetch_array($result)) {
-	$arr[$row['patient_id']]=$row;
-	
-    }
+       while($row =mysql_fetch_array($result)) 
+ 	$arr[$row['patient_id']]=$row;
+     
 	$patientDetailsArray['patientDetails']=$arr;
-			echo json_encode($patientDetailsArray);
-		//	$sql = "insert into patient (patient_id,firstname,lastname,age,gender,date_of_birth,phone) values(3,'mayank','ali','34','M','11-10-1986','7204219771')";
-
-  //  $result = mysql_query($sql);
+	echo json_encode($patientDetailsArray);
 	mysql_close();
 ?>
